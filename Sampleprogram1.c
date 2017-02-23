dcce8dc206b18e3744dc20266460a9f4f86bfed4
Keindae <noblettm@mail.gvsu.edu>
2017-02-23 14:24:03 -0500

    Updated

--- Sampleprogram1.c
+++ Sampleprogram1.c
@@ -26,19 +26,20 @@ int main (int argc, char **argv)
 
    //Things for semaphores
    int semid;
+   //Key to pass to semget()
    key_t key = 1234;
    struct sembuf op;
    semun_t arg;
 
    if((semid = semget(key, 1, IPC_CREAT | 0666)) == -1){
-       perror("semget: semget failed");
+       perror("semget - semget failed\n");
        exit(1);
    }
 
    //initaializes the semaphore in the set to a value of 1
    arg.val = 1;
    if(semctl(semid, 0, SETVAL, arg) < 0){
-       perror("semctl failed");
+       perror("semctl failed\n");
        exit(-1);
    }
 
@@ -57,28 +58,32 @@ int main (int argc, char **argv)
    shmPtr[0] = 0; 
    shmPtr[1] = 1;
 
+   //Signifies semaphore
    op.sem_num = 0;
+   //Wait until there is a lock on semaphore
    op.sem_flg = 0;
 
    if (!(pid = fork())) { 
       for (i=0; i<loop; i++) { 
+
          //Operation for the semaphore
          //This reduces the semaphore count to lock
           op.sem_op = -1; 
 
           if(semop(semid, &op, 1) == -1){
-              perror("semop failure");
+              perror("semop failure\n");
               exit(-1);
           }
-               // swap the contents of shmPtr[0] and shmPtr[1] 
+          // swap the contents of shmPtr[0] and shmPtr[1] 
           temp = shmPtr[0];
           shmPtr[0] = shmPtr[1];
           shmPtr[1] = temp;
 
           //release the semaphore
-          op.sem_op == 1;
+          op.sem_op = 1;
+
           if(semop(semid, &op,1) == -1){
-              perror("semop failure");
+              perror("semop failure\n");
               exit(-1);
           }
 
@@ -93,16 +98,18 @@ int main (int argc, char **argv)
       for (i=0; i<loop; i++) { 
           op.sem_op = -1;
           if(semop(semid, &op, 1) == -1){
-              perror("semop failure");
+              perror("semop failure\n");
               exit(-1);
             }
+
+          //Swaps the contents of shmPtr[0] and shmPtr[1]
           temp = shmPtr[0];
           shmPtr[0] = shmPtr[1];
           shmPtr[1] = temp;
 
           op.sem_op = 1;
           if(semop(semid, &op,1) == -1){
-              perror("Thread1: semop failure");
+              perror("Thread1: semop failure\n");
               exit(-1);
           }
       }
